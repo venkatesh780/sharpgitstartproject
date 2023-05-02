796f6abcb1367c39697275535f055e7d1c2bacbb
venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-05-02 14:53:06 +0530

    made 3rd item invisible and added backcolors for odd items

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const pageHeading = document.getElementById("header-title");
 const mainHeading = document.getElementsByClassName("title");
 const listItems = document.getElementsByClassName("list-group-item");
 const listItemsWithTagName = document.getElementsByTagName("li");
+const getOddElements = document.querySelectorAll(".list-group-item");
 
 listItemsWithTagName[4].setAttribute("class", "list-group-item");
 
@@ -11,10 +12,15 @@ pageHeading.style.borderBottom = "1px solid black";
 
 mainHeading[0].style.fontWeight = "bold";
 mainHeading[0].style.color = "green";
-listItems[2].style.backgroundColor = "green";
+// listItems[2].style.backgroundColor = "green";
+listItems[1].style.backgroundColor = "green";
 
 for (let i = 0; i < listItems.length; i++) {
   listItems[i].style.fontWeight = "bold";
 }
 
-console.log(listItemsWithTagName[4]);
+listItems[2].style.display = "none";
+
+for (let i = 0; i <= getOddElements.length; i += 2) {
+  getOddElements[i].style.backgroundColor = "green";
+}
