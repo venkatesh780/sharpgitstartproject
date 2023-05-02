b391d697f89bd574c466d54fb8ea63f6793d4a9c
venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-05-02 11:59:34 +0530

    modified add items heading

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const pageHeading = document.getElementById("header-title");
+const mainHeading = document.getElementsByClassName("title");
 pageHeading.textContent = "Sharpner List";
+
+pageHeading.style.borderBottom = "1px solid black";
+
+mainHeading[0].style.fontWeight = "bold";
+mainHeading[0].style.color = "green";
