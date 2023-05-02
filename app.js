7d18e901f1e0dc159e7f1076dd11cccd8f11182c
venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-05-02 14:17:22 +0530

    added fontweight and background for list itmes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const pageHeading = document.getElementById("header-title");
 const mainHeading = document.getElementsByClassName("title");
+const listItems = document.getElementsByClassName("list-group-item");
 pageHeading.textContent = "Sharpner List";
 
 pageHeading.style.borderBottom = "1px solid black";
 
 mainHeading[0].style.fontWeight = "bold";
 mainHeading[0].style.color = "green";
+listItems[2].style.backgroundColor = "green";
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = "bold";
+}
